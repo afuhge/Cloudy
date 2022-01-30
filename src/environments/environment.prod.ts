a998ffa967806e199304b7ce728b9780d6cfe15f
Annika Fuhge <annikafuhge96@gmail.com>
2022-01-30 19:43:34 +0100

    change prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   weatherForecastService: {
-    url: 'https://api.openweathermap.org/data/2.5/onecall',
+    url: 'http://api.openweathermap.org/data/2.5/onecall',
     key: 'abef800baf0f5c61c78b4ce85e3031e3',
   },
   cityService: {
