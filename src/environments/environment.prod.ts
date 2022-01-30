ed65cef0b4a34cf38bacc17cc3da0c678e040f53
Annika Fuhge <annikafuhge96@gmail.com>
2022-01-30 18:14:01 +0100

    add missing stuff in environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherForecastService: {
+    url: 'https://api.openweathermap.org/data/2.5/onecall',
+    key: 'abef800baf0f5c61c78b4ce85e3031e3',
+  },
+  cityService: {
+    url: 'http://api.openweathermap.org/geo/1.0/direct',
+    key: 'abef800baf0f5c61c78b4ce85e3031e3',
+  },
 };
